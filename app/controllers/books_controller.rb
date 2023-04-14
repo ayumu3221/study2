9dafb5bc55af80603fe4002c79ae8456a55051e2
EC2 Default User <117354218+ayumu3221@users.noreply.github.com>
2023-04-14 10:58:30 +0000

    @book = Book.find(params[:id])削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,11 +23,9 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -36,7 +34,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
